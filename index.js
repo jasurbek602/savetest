22c780d8c78bf4200797eac727f63661b71bbc10
jasurbek602 <115354796+jasurbek602@users.noreply.github.com>
2025-05-18 18:11:52 +0500

    Update index.js

--- index.js
+++ index.js
@@ -56,6 +56,7 @@ bot.on('message', (msg) => {
 
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
+  
   console.log('Fayl yuborilmoqda:', file.message_id);
 console.log('Guruh ID:', FILE_GROUP_ID);
 console.log('Fayl nomi:', file.file_name);
